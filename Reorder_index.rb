f44550caa1ac2b499198ac80672214405cc7ee24
unknown <rodolfcs@gmail.com>
2023-09-23 20:13:05 -0300

    reorder

---
+++ Reorder_index.rb
@@ -0,0 +1,12 @@
+
+array = [8, 5, 6, 2, 9, 4]
+arr_s = arr.sort
+puts arr.map{|e| arr_s.index(e)}
+
+
+arr = [2, 5, 4, 7, 5, 9]
+arr_s = arr.sort
+puts arr.map{|e| arr_s.index(e)}
+
+
+                                           
